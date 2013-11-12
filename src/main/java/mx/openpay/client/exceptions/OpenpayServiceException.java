6b7fbf7d7304161d431e0f7b0bfc77dca1382ca5
Eli Carlos Lopez Rangel <eli.lopez@opencard.mx>
2013-11-12 12:28:51 -0600

    Cambiado nombre de clase

--- src/main/java/mx/openpay/client/exceptions/OpenpayServiceException.java
+++ src/main/java/mx/openpay/client/exceptions/OpenpayServiceException.java
@@ -9,7 +9,7 @@ import com.google.gson.annotations.SerializedName;
 @Getter
 @Setter
 @ToString
-public class HttpError extends Exception {
+public class OpenpayServiceException extends Exception {
 
     private static final long serialVersionUID = -7388627000694002585L;
 
@@ -22,23 +22,23 @@ public class HttpError extends Exception {
 
     private String requestId;
 
-    public HttpError() {
+    public OpenpayServiceException() {
         super();
     }
 
-    public HttpError(final String message) {
+    public OpenpayServiceException(final String message) {
         super(message);
     }
 
-    public HttpError(final Throwable cause) {
+    public OpenpayServiceException(final Throwable cause) {
         super(cause);
     }
 
-    public HttpError(final String message, final Throwable cause) {
+    public OpenpayServiceException(final String message, final Throwable cause) {
         super(message, cause);
     }
 
-    public HttpError(final String message, final Throwable cause, final boolean enableSuppression,
+    public OpenpayServiceException(final String message, final Throwable cause, final boolean enableSuppression,
             final boolean writableStackTrace) {
         super(message, cause, enableSuppression, writableStackTrace);
     }
