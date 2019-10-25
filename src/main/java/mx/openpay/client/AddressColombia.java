4b7c9b67142050c81659d2ce2e28d9ed2f93f0da
Bruce RIP <49887504+BrunoRiveraPina@users.noreply.github.com>
2019-10-25 17:09:08 -0500

    Create AddressColombia.java
    Agrego objeto de direccion para clientes colombianos

---
+++ src/main/java/mx/openpay/client/AddressColombia.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package mx.openpay.client;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+/**
+ * @author bruno.rivera
+ *
+ */
+@Getter
+@Setter
+@ToString
+public class AddressColombia {
+
+	private String department;
+	private String city;
+	private String additional;
+}
