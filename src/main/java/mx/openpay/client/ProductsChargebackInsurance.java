66518fcce853e1009b85cd920205379e32e2172d
DEV-MAURICIO-MORENO <mauricio.moreno@DEV-MAURICIO-MORENO.local>
2020-11-05 13:24:41 -0600

    se eliminan las dependencias de Jackson para la clase productsChargeBackInsurance

--- src/main/java/mx/openpay/client/ProductsChargebackInsurance.java
+++ src/main/java/mx/openpay/client/ProductsChargebackInsurance.java
@@ -1,11 +1,8 @@
 package mx.openpay.client;
 
-import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import com.google.gson.annotations.SerializedName;
 import lombok.Getter;
 import lombok.Setter;
-import mx.openpay.commons.json.BigDecimalJsonSerializer;
-import mx.openpay.commons.json.DefaultJsonSerialization;
 
 import java.math.BigDecimal;
 
@@ -19,8 +16,7 @@ public class ProductsChargebackInsurance {
 
     @SerializedName("name")
     private String productName;
-
-    @JsonSerialize(using = BigDecimalJsonSerializer.class)
+    
     @SerializedName("price")
     private BigDecimal productPrice;
 
