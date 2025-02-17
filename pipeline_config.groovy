6a4640a47dfcc93e6ff27c3a10c4d89533eeb10c
CARLOS-GALVAN <carlos.galvan@openpay.mx>
2025-02-17 10:28:50 -0600

    Se agrega pipeline_config

---
+++ pipeline_config.groovy
@@ -0,0 +1,14 @@
+/**
+  Estas configuraciones son especificas para este repositorio/proyecto
+*/
+/** Configuraciones generales */
+jdk_tool = 'Java-OP11'
+
+jte {
+    /** pipeline template a usar */
+    pipeline_template = "jar_library"
+}
+/** Librerias que se usaran */
+libraries{
+    maven
+}
