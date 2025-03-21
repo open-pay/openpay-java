eacab8f599d32b401643a88386f985904024008d
CARLOS-GALVAN <carlos.galvan@openpay.mx>
2025-03-21 11:34:45 -0600

    Correccion prueba unitara

--- src/test/integration/mx/openpay/core/client/full/MerchantCardsTest.java
+++ src/test/integration/mx/openpay/core/client/full/MerchantCardsTest.java
@@ -123,7 +123,7 @@ public class MerchantCardsTest extends BaseTest {
         this.api.cards().update(new UpdateCardParams()
               .cardId(card.getId())
               .holderName("Jorge Rodriguez")
-              .expirationYear(30)
+              .expirationYear(25)
               .expirationMonth(2)
               .cvv2("222")
               );
